30dab98fa2214793cbb93184d4fdc99156e36a89
bialocatelli <beatrizglocatelli@gmail.com>
2022-04-28 11:50:02 -0300

    Criando componente Home e estilizando no css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://img.freepik.com/fotos-gratis/constelacao-de-peixes-no-contexto-do-ceu-noturno-os-elementos-desta-imagem-foram-fornecidos-por_496756-723.jpg" alt="Imagem da Tela Inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
+
