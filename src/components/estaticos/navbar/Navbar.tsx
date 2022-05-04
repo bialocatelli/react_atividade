31fbd93cf6b2f4fa873b7009ef9c9b47d129dc81
bialocatelli <beatrizglocatelli@gmail.com>
2022-05-04 05:52:47 -0300

    Criando sistema de rotas para Home e Login

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {AppBar, Box, Toolbar, Typography} from '@material-ui/core';
+import { Link } from 'react-router-dom'
+
+import './Navbar.css'
 
 
 function Navbar (){
@@ -14,45 +17,45 @@ function Navbar (){
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-    
+                        <Link to="/home" className="text-decorator-none">
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
                                     Home
                                 </Typography>
                             </Box>
-                        
+                        </Link>
 
-                        
+                        <Link to="/posts" className="text-decorator-none">
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
                                     Postagens
                                 </Typography>
                             </Box>
-                       
+                        </Link>
 
-                        
+                        <Link to="/temas" className="text-decorator-none">
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
                                     Temas
                                 </Typography>
                             </Box>
-                        
+                        </Link>
 
-                        
+                        <Link to="/formularioTema" className="text-decorator-none">
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
                                     Cadastrar Temas
                                 </Typography>
                             </Box>
-                        
+                        </Link>
 
-                        
+                        <Link to="/login" className='text-decorator-none'>
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
                                     Logout
                                 </Typography>
                             </Box>
-                        
+                        </Link>
                     </Box>
 
                 </Toolbar>
