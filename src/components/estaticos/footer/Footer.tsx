e146915be4927e9f7725d1a15242f6602314107b
bialocatelli <beatrizglocatelli@gmail.com>
2022-05-06 05:50:54 -0300

    Implementando a segurança no projeto

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, Grid, Typography } from '@material-ui/core'
-import InstagramIcon from '@material-ui/icons/Instagram'
-import FacebookIcon from '@material-ui/icons/Facebook'
+import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import './Footer.css';
 
@@ -12,20 +11,16 @@ function Footer() {
                 <Grid alignItems="center" item xs={12}>
                     <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Me siga nas redes sociais </Typography>
                         </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                <FacebookIcon className='redes'/>
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                <InstagramIcon className='redes'/>
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <LinkedInIcon className='redes'/>
-                            </a>
-                        </Box>
+                        <a href="https://github.com/bialocatelli" target="_blank">
+                            <GitHubIcon className='redes' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/beatriz-gon%C3%A7alves-locatelli-b01759209/" target="_blank">
+                            <LinkedInIcon className='redes' />
+                        </a>
                     </Box>
+
                     <Box className='box2'>
                         <Box paddingTop={1}>
                             <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2020 Copyright:</Typography>
