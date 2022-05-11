6f1a939d00966da5a6d2b90c9a0e1c9b4c445841
bialocatelli <beatrizglocatelli@gmail.com>
2022-05-11 02:37:56 -0300

    Crud - implementando o Post, Put e Delete da model de Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
-import Home from './paginas/home/Home';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
 import Login from './paginas/login/Login';
-
-import './App.css';
-
+import Home from './paginas/home/Home';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
@@ -19,12 +23,29 @@ function App() {
       <div style={{ minHeight: '100vh' }}>
 
         <Routes>
-        <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login />} />
+
           <Route path="/login" element={<Login />} />
+
           <Route path="/home" element={<Home />} />
+
           <Route path="/cadastro" element={<CadastroUsuario />} />
+
           <Route path="/temas" element={<ListaTema />} />
+
           <Route path="/posts" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
         </Routes>
 
       </div>
